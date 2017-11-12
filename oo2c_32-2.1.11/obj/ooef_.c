77529db4b33a471432f301f94990d2f89a5e62bb
GunterMueller <gunter.mueller@gmail.com>
2017-11-12 17:17:51 -0500

    Corrected initial commit.

--- oo2c_32-2.1.11/obj/ooef_.c
+++
@@ -1,7 +0,0 @@
-#include <RT0.oh>
-int main (int argc, char *argv[]) {
-  RT0__argc = (OOC_INT32)argc;
-  RT0__argv = (OOC_CHAR8**)argv;
-  OOC_ooef_open(NULL);
-  return 0;  /* _program_exit_code */
-}
